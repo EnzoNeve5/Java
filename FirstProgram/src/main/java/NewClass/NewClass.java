cf3605977073514f7234407a9d12a79bc9b16ac0
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2022-09-22 16:06:34 -0300

    Add files via upload

---
+++ FirstProgram/src/main/java/NewClass/NewClass.java
@@ -0,0 +1,12 @@
+package NewClass;
+
+public class NewClass {
+
+    public static void main(string[] args) {
+        System.out.println("Olá, Mundo!");
+    }
+
+    private NewClass() {
+    }
+
+}
