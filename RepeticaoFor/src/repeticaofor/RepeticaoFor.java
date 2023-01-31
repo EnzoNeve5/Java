7df09631c0a723cc1de80fb9122338c273588cb9
EnzoNeve5 <111204969+EnzoNeve5@users.noreply.github.com>
2023-01-31 17:08:54 -0300

    Add files via upload

---
+++ RepeticaoFor/src/repeticaofor/RepeticaoFor.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package repeticaofor;
+
+/**
+ *
+ * @author root
+ */
+public class RepeticaoFor {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int cc=15; cc>=5; cc-=2) {
+            System.out.println(cc);
+        }
+        /*
+        
+        int cc=15;
+        while (cc>=15) {
+            System.out.println("Cambalhota");
+            cc-=2;
+        }
+        
+        */
+    }
+    
+}
